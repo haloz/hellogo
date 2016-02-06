41c2fffbcb49979400338f2d641f3ffcc13fa40b
haloz <haloz@gmx.net>
2016-02-06 14:48:26 +0100

    casting

--- 1-helloworld/two.go
+++ 1-helloworld/two.go
@@ -23,12 +23,27 @@ func named_return_values(x int) (y, z int) {
 	return
 }
 
+func basictypes() {
+	var (
+		t bool = true
+		MaxInt uint64 = 1<<64-1
+	)
+	fmt.Printf("type: %T, value: %v\n", t,t)
+	fmt.Printf("type: %T, value: %v\n", MaxInt,MaxInt)
+}
+
 func main() {
 	s := "number" // short variable declaration
 	fmt.Printf("Have a %s: %g\n", s, rand.Intn(10))	
 	fmt.Printf("Squarepants! %g\n", math.Sqrt(42))
-	fmt.Println(math.Pi) 
+	fmt.Println(math.Pi) 		
 	fmt.Println(add(23, 19))
 	fmt.Println(add_shorter(19, 23))
 	fmt.Println(named_return_values(4))	
+	basictypes()
+
+	var f float64 = math.Sqrt(55)
+	fmt.Println(f)
+	var fr = uint(f)
+	fmt.Println(fr)
 }
